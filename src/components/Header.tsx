f5628a164709a822cdf662bb4b7d9bfa39f62399
Bhuneshar <imankitkalirawana@gmail.com>
2023-11-22 16:55:41 +0530

    Add sidebar functionality for mobiles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
 import "../css/Header.css";
 import { Link } from "react-router-dom";
 import data from "../Data";
+import { useState } from "react";
 
 const Header = () => {
+  // sidebar functionality
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <>
       <header className="header">
@@ -72,10 +80,63 @@ const Header = () => {
           </a>
         </div>
         {/* Toggler */}
-        <div className="toggler">
-          <i className="fa-regular fa-bars-staggered"></i>
+        <div className="toggler" onClick={toggleSidebar}>
+          <i
+            className={`fa-regular ${
+              sidebarOpen ? "fa-xmark " : ""
+            }fa-bars-staggered`}
+          ></i>
         </div>
       </header>
+
+      {/* sidebar for mobiles */}
+      <aside
+        className="sidebar"
+        style={{
+          transform: sidebarOpen ? "translateX(0)" : "translateX(-100%)",
+        }}
+      >
+        {/* logo */}
+        <Link to="/" className="logo">
+          <img className="logo-icon" src="/logo-dark.png" alt="logo" />
+          <h1 className="logo-text">
+            Devo<span className="color">Code</span>
+          </h1>
+        </Link>
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+            <Link to="/" className="sidebar-list-link">
+              <i className="fa-regular fa-home-lg-alt"></i>
+              Home
+            </Link>
+          </li>
+          <li className="sidebar-list-item">
+            <a
+              className="sidebar-list-link"
+              href="https://divinelydeveloper.me#contact"
+            >
+              <i className="fa-regular fa-envelope"></i>
+              Contact
+            </a>
+          </li>
+          <li className="sidebar-list-item">
+            <a
+              className="sidebar-list-link"
+              href="https://divinelydeveloper.me"
+            >
+              <i className="fa-regular fa-user"></i>
+              About
+            </a>
+          </li>
+          {/* resouces dropdown */}
+          <li className="sidebar-list-item">
+            <Link to="/resources" className="sidebar-list-link">
+              <i className="fa-regular fa-book"></i>
+              Resources
+            </Link>
+          </li>
+        </ul>
+      </aside>
     </>
   );
 };
