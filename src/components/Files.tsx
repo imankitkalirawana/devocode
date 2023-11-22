54ab5ae3412c9c7a5f4266822f5b30ff90231152
Bhuneshar <imankitkalirawana@gmail.com>
2023-11-22 10:39:59 +0530

    Refactor file breadcrumbs in Files component

--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import data from "../Data";
+
 interface Subject {
   id: string;
   code: string;
@@ -29,7 +30,7 @@ const Files: React.FC = () => {
     return <h1>Not defined</h1>;
   }
 
-  const selectedFile = (selectedSubject as any)[fileName] as File[];
+  const selectedFile = (selectedSubject as any)[fileName] as File[] | undefined;
 
   if (!selectedFile) {
     return <h1>No Data found</h1>;
@@ -39,22 +40,16 @@ const Files: React.FC = () => {
     <>
       <div className="section resources">
         {/* breadcrumbs */}
-
         <div className="breadcrumbs">
-          <Link className="breadcrumbs-item" to="/">
-            Home
-          </Link>
-          <i className="fas fa-chevron-right breadcrumbs-item"></i>
-          <Link className="breadcrumbs-item" to="/resources">
-            Subjects
-          </Link>
-          <i className="fas fa-chevron-right breadcrumbs-item"></i>
-          <Link className="breadcrumbs-item" to={`/resources/${subjectCode}`}>
-            {subjectCode}
-          </Link>
-          <i className="fas fa-chevron-right breadcrumbs-item"></i>
-          <span className="breadcrumbs-item selected">{fileName}</span>
+          <Link to="/">Home</Link> /{" "}
+          <Link to={`/subjects/${selectedSubject.code}`}>
+            {selectedSubject.title}
+          </Link>{" "}
+          / {fileName}
+          <div className="clear"></div>
         </div>
+
+        <div className="breadcrumbs">{/* ... (unchanged) */}</div>
         <h2 className="section-title">{fileName}</h2>
         <div className="section-content">
           <div className="section-menu">
